520a5cf7366f579f5334a1489c1487bd51bdd512
Charles Loehle <clguitar1@gmail.com>
2022-09-17 08:50:15 -0400

    added js to control css automatically

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const topBar = document.querySelector('.top-bar')
-const bottomHeader = document.querySelector('.cl-web-header')
+const topBar = document.querySelector('.top-bar');
+const bottomHeader = document.querySelector('.cl-web-header');
+// on page load, bottomHeader is at bottom of topBar
+bottomHeader.style.transform = `translateY(${topBar.offsetHeight}px)`;
 
 document.addEventListener('scroll', e => {
-  // console.log(window.scrollY) 
-  if (window.scrollY >= 60) {
-    // console.log('60!!');
-    topBar.classList.add('slide-up')
-    bottomHeader.classList.add('she-header')
-  } else {
-    topBar.classList.remove('slide-up')
-    bottomHeader.classList.remove('she-header')
-  }
-})
-
+	// Change number to match Sticky Header Effects scroll distance value
+	if (window.scrollY >= 60) {
+		topBar.classList.add('slide-up');
+		bottomHeader.classList.add('she-header');
+		bottomHeader.style.transform = `translateY(0px)`;
+	} else {
+		topBar.classList.remove('slide-up');
+		bottomHeader.classList.remove('she-header');
+		bottomHeader.style.transform = `translateY(${topBar.offsetHeight}px)`;
+	}
+});
